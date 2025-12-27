5af0c457ac1dca410995bf9d35b01972986f20cb
hank9999 <hank9999@qq.com>
2025-12-28 01:04:28 +0800

    refactor: remove unused verify_crc32 method and tests

--- src/kiro/parser/crc.rs
+++ src/kiro/parser/crc.rs
@@ -18,18 +18,6 @@ pub fn crc32(data: &[u8]) -> u32 {
     CRC32.checksum(data)
 }
 
-/// 验证 CRC32 校验和
-///
-/// # Arguments
-/// * `data` - 要验证的数据
-/// * `expected` - 期望的校验和值
-///
-/// # Returns
-/// 如果校验和匹配返回 true，否则返回 false
-pub fn verify_crc32(data: &[u8], expected: u32) -> bool {
-    crc32(data) == expected
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,13 +33,5 @@ mod tests {
         // "123456789" 的 CRC32 (ISO-HDLC) 值是 0xCBF43926
         let data = b"123456789";
         assert_eq!(crc32(data), 0xCBF43926);
-    }
-
-    #[test]
-    fn test_verify_crc32() {
-        let data = b"test data";
-        let checksum = crc32(data);
-        assert!(verify_crc32(data, checksum));
-        assert!(!verify_crc32(data, checksum + 1));
     }
 }
